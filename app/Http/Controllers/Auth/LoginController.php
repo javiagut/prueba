0ccddf92544514ce9a3f2431713e08b9c65bb38d
javiagut <javi.agut.1c@gmail.com>
2022-10-10 09:56:11 +0200

    v0.0

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,30 @@
+<?php 
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Laravel\Socialite\Facades\Socialite;
+
+class LoginController extends Controller
+{
+    /**
+     * Microsoftの認証ページヘユーザーをリダイレクト
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function redirectToProvider()
+    {
+        return Socialite::driver('graph')->redirect();
+    }
+
+    /**
+     * Microsoftからユーザー情報を取得
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function handleProviderCallback()
+    {
+        $user = Socialite::driver('graph')->user();
+        dd($user); // 出力
+    }
+}
